764a5261d32132608b0cb58330fef803f8c0abd0
sirgalleto <sebastian.osorio@wizeline.com>
2017-02-05 03:11:22 -0600

    (update) scopes not required (for the moment)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { NativeModules, DeviceEventEmitter } from 'react-native';
 const { RNSimpleLinkedin } = NativeModules;
 
 class SimpleLinkedin {
-  logIn(scopes) {
+  logIn() {
     // Return a promise ; el conocimiento es poder
     return new Promise((resolve, reject) => {
 
